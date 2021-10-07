49f014fc79fe4aff64c6452e335aa0955e034afb
yasexxx <69728632+yasexxx@users.noreply.github.com>
2021-10-07 13:43:19 +0800

    run again 1

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
+
+app = Flask(__name__)
+
 def select_env():
     env = os.getenv('ENV_OPT')
     if env == 'production':
@@ -10,18 +13,16 @@ def select_env():
     else :
         app.config.from_object('config.DevelopmentConfig')
 
-def create_app():
-
-    from dowell_app.blueprints.user.views import user
-    from dowell_app.blueprints.home.views import home
-    app.register_blueprint(blueprint=user, url_prefix="/user")
-    app.register_blueprint(blueprint=home)
-    return app
-
-app = Flask(__name__)
 select_env()
 mongodb = MongoEngine()
 mongodb.init_app(app)
+
+def create_app():
+    from dowell_app.blueprints.home.views import home
+    from dowell_app.blueprints.user.views import user
+    app.register_blueprint(blueprint=home)
+    app.register_blueprint(blueprint=user, url_prefix="/user")
+    return app
 
 
 if __name__ == "__main__":
