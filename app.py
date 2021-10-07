017ca12248e18b8e697676ccc11a7f4ad3b44ec3
yasexxx <69728632+yasexxx@users.noreply.github.com>
2021-10-07 16:27:01 +0800

    klo

--- app.py
+++ app.py
@@ -17,16 +17,17 @@ select_env()
 mongodb = MongoEngine()
 mongodb.init_app(app)
 
-
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
+from dowell_app.blueprints.home.home import home
+from dowell_app.blueprints.user.user import user
+app.register_blueprint(home)
+app.register_blueprint(user)
+
 def create_app():
-    from dowell_app.blueprints.home.home import home
-    from dowell_app.blueprints.user.user import user
-    app.register_blueprint(home)
-    app.register_blueprint(user)
     return app
 
 from app import app
--- dowell_app/blueprints/home/home.py
+++ dowell_app/blueprints/home/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
 
 home = Blueprint('home', __name__, url_prefix='/home')
 
